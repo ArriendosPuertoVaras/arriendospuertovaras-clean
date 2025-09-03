0bfb3a2b1ca7eccac68060394e2cffb016cd09f4
ArriendosPuertoVaras <ricardoperaltavargas@gmail.com>
2025-09-03 14:40:02 -0400

    feat(routes): activar Properties y Services

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
-// Páginas que faltaban
 
-import Properties from "./pages/Properties.jsx";
-import ServicesHub from "./pages/ServicesHub.jsx";
 import Layout from "./pages/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import Contact from "./pages/Contact.jsx";
@@ -10,6 +7,10 @@ import Dashboard from "./pages/Dashboard.jsx";
 import AdminDashboard from "./pages/AdminDashboard.jsx";
 import Blog from "./pages/Blog.jsx";
 import HelpCenter from "./pages/HelpCenter.jsx";
+
+// NUEVO
+import Properties from "./pages/Properties.jsx";
+import ServicesHub from "./pages/ServicesHub.jsx";
 
 // HashRouter en local, BrowserRouter en prod
 const useHash = import.meta?.env?.VITE_USE_HASH === "true";
@@ -19,22 +20,22 @@ export default function App() {
   return (
     <RouterImpl>
       <Routes>
-        {/* Envolvemos todas las rutas con tu Layout */}
         <Route element={<Layout />}>
-        {/* Propiedades */}
-          <Route path="/properties/*" element={<Properties />} />
-          <Route path="/propiedades/*" element={<Properties />} />
-
-           {/* Servicios */}
-          <Route path="/services/*" element={<ServicesHub />} />
-          <Route path="/servicios/*" element={<ServicesHub />} />
-
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/dashboard/*" element={<Dashboard />} />
           <Route path="/admin/*" element={<AdminDashboard />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/help" element={<HelpCenter />} />
+
+          {/* NUEVO: Propiedades (es/en) */}
+          <Route path="/properties/*" element={<Properties />} />
+          <Route path="/propiedades/*" element={<Properties />} />
+
+          {/* NUEVO: Servicios (es/en) */}
+          <Route path="/services/*" element={<ServicesHub />} />
+          <Route path="/servicios/*" element={<ServicesHub />} />
+
           <Route path="*" element={<Home />} />
         </Route>
       </Routes>
